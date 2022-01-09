53f80a6225fde7749fa849d47eea1bbb49cfd068
cskcsimha <92619094+cskcsimha@users.noreply.github.com>
2022-01-09 11:16:05 +0530

    Add files via upload

--- Face Recognition Project/Image_recognizer/main.js
+++ Face Recognition Project/Image_recognizer/main.js
@@ -19,3 +19,18 @@ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/T
 function modelLoaded(){
     console.log('model Loaded');
 }
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
